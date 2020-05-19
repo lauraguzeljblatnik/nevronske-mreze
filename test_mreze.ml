ae7f0930377eace5568c34fb5a6f0217d3360698
lauraguzeljblatnik <laura.guzelj-blatnik@student.fmf.uni-lj.si>
2020-05-19 10:33:27 +0200

    test

--- test_mreze.ml
+++ test_mreze.ml
@@ -1,7 +1,8 @@
 #use "nevronske_mreze.ml";;
 #use "csv_ocaml.ml";;
 	
-let topology = [|11;3;5;3|]	
+let topology = [|11;5;15;6;3|]	
+let rate = 0.01
 
 let network = initialize_network topology 
 
@@ -16,12 +17,16 @@ let std_i = std input mean_i
 
 let mean_o = mean out	
 let std_o = std out mean_o
-	
-let trained_weights = train_network input out topology 0.001 1.0 sigmoid d_sigmoid
 
-let trained_w_input = train_with_input_weights input out topology 0.1 w1 sigmoid d_sigmoid
+let norm_test = norm_z_score test_examples.(10) mean_i std_i	
 
-let norm_test = norm_z_score test_examples.(10) mean_i std_i
+let prediction_unlearned = predict norm_test network w sigmoid
+let denorm_pred_unlearned = denorm_z_score prediction_unlearned mean_o std_o
+
+
+let trained_weights = train_network input out topology rate 1.0 sigmoid d_sigmoid
+
+let trained_w_input = train_with_input_weights input out topology rate w1 sigmoid d_sigmoid
 
 (* let prediction_unlearned1 = predict norm_test network w1 sigmoid *)
 (* let denorm_pred_unlearned1 = denorm_z_score prediction_unlearned1 mean_o std_o *)
@@ -31,9 +36,6 @@ let denorm_pred_learned_input = denorm_z_score prediction_learned_input mean_o s
 
 let prediction_learned = predict norm_test network trained_weights sigmoid
 let denorm_pred_learned = denorm_z_score prediction_learned mean_o std_o
-
-let prediction_unlearned = predict norm_test network w sigmoid
-let denorm_pred_unlearned = denorm_z_score prediction_unlearned mean_o std_o
 
 (* let prediction1 = predict test_examples.(0) network top1 trained_weights sigmoid *)
 
